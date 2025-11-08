be3d9c572621134bb00d71fda4fa72ca26830089
ben-spr <benjamin.sprick@gmx.de>
2025-11-08 20:15:58 +0100

    Add Java class constructor with slot initialization.

--- towers_of_hanoi/towers.java
+++ towers_of_hanoi/towers.java
@@ -1,20 +1,28 @@
+import java.util.ArrayDeque;
 
-public class TowerGame{
+class TowerGame{
     int disc_number;
-    int[][] slot = {
-            {},
-            {},
-            {},
-    };
+    ArrayDeque<Integer>[] slots;
 
     TowerGame(int num_discs){
-        disc_number = num_discs;
+        this.disc_number = num_discs;
+        this.slots = new ArrayDeque[]{
+                new ArrayDeque<Integer>(),
+                new ArrayDeque<Integer>(),
+                new ArrayDeque<Integer>(),
+        };
+        for (int i = num_discs; i > 0; i--) {
+//            System.out.println("Attempting to add to slots[0], i=" + i);
+            this.slots[0].addLast(i);
+        };
+//      System.out.println(String.format("Disc number set: %d", this.disc_number));
+//        System.out.println(String.format("Slot 0 set: %s", this.slots[0].toString()));
     }
-    
+
 }
 
-class towers(){
-    public static void main(String[] args) {
-        TowerGame game = TowerGame(4);
+class towers{
+    public static void main (String[] args) {
+        TowerGame game = new TowerGame(4);
     }
 }
